f7623ee5e3dc6730f462376d52f181767138be54
jheloper9102 <jheloper9102@gmail.com>
2018-12-16 16:36:40 +0900

    Duck 추상 클래스에 Quackable, Flyable 인터페이스를 인스턴지스 변수로 가도록 수정.

--- src/main/java/hfd/strategy/Duck.java
+++ src/main/java/hfd/strategy/Duck.java
@@ -2,12 +2,21 @@ package hfd.strategy;
 
 public abstract class Duck {
 
-    public static final String QUACK = "quack!!!";
     public static final String SWIM = "swimming!!!";
-    public static final String FLY = "flying!!!";
+
+    public Quackable quackBehavior;
+    public Flyable flyBehavior;
 
     public String swim() {
         return SWIM;
+    }
+
+    public String performQuack() {
+        return quackBehavior.quack();
+    }
+
+    public String performFly() {
+        return flyBehavior.fly();
     }
 
     public abstract String display();
