d07fa5407b0c318f1b5be4f84b21eecbaa6c970b
jheloper9102 <jheloper9102@gmail.com>
2018-12-16 15:34:12 +0900

    오리 중에 고무오리도 있으므로 고무오리 클래스 및 테스트 코드 추가.

---
+++ src/main/java/hfd/strategy/RubberDuck.java
@@ -0,0 +1,11 @@
+package hfd.strategy;
+
+public class RubberDuck extends Duck {
+
+    public static final String DISPLAY = "this is RubberDuck!";
+
+    @Override
+    public String display() {
+        return DISPLAY;
+    }
+}
